6f7f6fb1817d6856931992c2cd6361ae14f49f57
Diego Rocha <diego.rocha@neon.com.br>
2020-06-14 10:39:52 -0300

    refactor(lottie): swap lottie component for native lib

--- src/components/Preloader/index.tsx
+++ src/components/Preloader/index.tsx
@@ -1,31 +1,36 @@
-import React from 'react';
-import Lottie from 'react-lottie';
+import React, { useEffect } from 'react';
+import Lottie, { AnimationConfigWithData } from 'lottie-web';
 
 import { RootState } from 'reducers';
 import { useSelector } from 'react-redux';
-import { PreloaderContainer, PreloaderWrapper } from './styles';
-
-interface IPreloaderProps {
-  isActive: boolean;
-}
-
-const animationData = require('assets/animations/loading.json');
+import animationData from 'assets/animations/loading.json';
+import {
+  PreloaderContainer,
+  PreloaderWrapper,
+  PreloaderAnimationContainer,
+} from './styles';
 
 const Preloader: React.FC = (props) => {
   const preloaderState = useSelector((state: RootState) => state.preloader);
-  const preloaderAnimationData = {
-    animationData,
-    loop: true,
-    autoplay: true,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice',
-    },
-  };
+
+  useEffect(() => {
+    const container = document.getElementById(
+      'lottie-animation-container'
+    ) as Element;
+    const preloaderAnimationData: AnimationConfigWithData = {
+      container,
+      animationData,
+      loop: true,
+      autoplay: true,
+      renderer: 'canvas',
+    };
+    Lottie.loadAnimation(preloaderAnimationData);
+  }, []);
 
   return (
     <PreloaderContainer isActive={preloaderState.isPreloaderVisible}>
       <PreloaderWrapper>
-        <Lottie options={preloaderAnimationData} width={115} height={115} />
+        <PreloaderAnimationContainer id="lottie-animation-container" />
       </PreloaderWrapper>
     </PreloaderContainer>
   );
